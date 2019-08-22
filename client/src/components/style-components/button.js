86ccae9997dce3570755dc511ceecf540f427a79
Noam Tiram <Noam.Tiram.4@gmail.com>
2019-08-22 16:52:44 +0300

    front - styled stage

---
+++ client/src/components/style-components/button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+  color: black;
+  font-size: 1.4rem;
+  background: transparent;
+  margin: 1em;
+  padding: 0.25em 1em;
+  
+  border-radius: 3px;
+  &:hover{
+  background:var(--lightBlue);
+  }
+  &:focus {
+    border: 2px solid palevioletred;
+  }
+`;
