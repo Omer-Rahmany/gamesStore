f246ef201436d9997f8b4c9118f278bdb77a0323
Noam Tiram <Noam.Tiram.4@gmail.com>
2019-08-22 16:57:34 +0300

    adding home

---
+++ client/src/components/Home.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Navbar from './Navbar';
+import ProductList from './ProductList';
+import Details from './Details';
+import Cart from './Cart';
+import Default from './Default';
+import '../css/shopping.css';
+import {Switch,Route} from "react-router-dom";
+
+class Home extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/home" component={ProductList} />
+                    <Route path="/details" component={Details} />
+                    <Route path="/cart" component={Cart} />
+                    <Route component={Default} />
+                </Switch>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Home;
